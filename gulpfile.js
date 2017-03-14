6751bf1b7a1e6d3e2ee3d36440c57c54d8a858c3
Zhenyas-git <zhenkatanka@gmail.com>
2017-03-14 17:41:18 +0200

    Добавил автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,71 @@ var browserSync = require("browser-sync").create();
 var plumber = require("gulp-plumber");
 var autoprefixer = require("autoprefixer");
 var postcss = require("gulp-postcss");
+var mqpacker = require("css-mqpacker");
+var cssmin = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
+var svgstore = require("gulp-svgstore");
+var secuence = require("run-sequence");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
     gulp.src("less/style.less")
         .pipe(plumber())
         .pipe(less())
-        .pipe(postcss([autoprefixer({browsers: ["last 2 versions"]})]))
-        .pipe(gulp.dest("css"))
+        .pipe(postcss([autoprefixer({browsers: ["last 2 versions"]}),
+                      mqpacker({
+                          sort: true
+                      })
+                      ]))
+        .pipe(gulp.dest("build/css"))
+        .pipe(cssmin())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
         .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task("images", function () {
+   gulp.src("build/img/**/*.{png,jpg,gif}") 
+    .pipe(imagemin([
+       imagemin.optipng({optimizationLevel: 3}),
+       imagemin.jpegtran({progressive: true})
+   ]))
+    .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("svgsprite", function () {
+    return gulp.src("build/img/icons/*.svg")
+        .pipe(svgmin())
+        .pipe(svgstore({
+        inlineSvg: true
+    }))
+        .pipe(rename("symbols.svg"))
+        .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("copy", function() {
+   gulp.src([
+       "fonts/**/*.{woff,woff2}",
+       "img/**",
+       "js/**",
+       "*.html"
+       ], {
+       base: "."
+   })
+   .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+    del("build");
 });
 
 gulp.task("serve", function() {
     browserSync.init({
         server:{
-            baseDir: "./"
+            baseDir: "build"
         }
     });
 
@@ -25,4 +76,6 @@ gulp.task("serve", function() {
     gulp.watch("*.html").on("change", browserSync.reload);
 });
 
-gulp.task("default", ["style", "serve"]);
+gulp.task("build", function(fn) {
+    run("clean","copy" ,"style", "images", "svgsprite", fn);
+});
